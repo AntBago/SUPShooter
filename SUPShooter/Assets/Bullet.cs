9e1bf310768aabe9d0ad6731a3fc8322a7135bdd
Eddeboiii <edvin.ekstrand@hotmail.com>
2021-03-16 10:13:47 +0100

    shooting 2
    Skotten flyger och träffar monstret

---
+++ SUPShooter/Assets/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public int damage = 25;
+    public Rigidbody2D rb;
+    
+
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+        
+    }
+
+    void OnTriggerEnter2D (Collider2D hitInfo) 
+    {
+        Monster monster = hitInfo.GetComponent<Monster>();
+        if (monster !=null)
+        {
+            monster.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
