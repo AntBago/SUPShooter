7f458b7913f0c0936199a1afddb42a5020609374
mattefan <mattias.osterberg@live.com>
2021-03-18 13:58:25 +0100

    Ny gubbe
    Arm rotation
    Animering på gubbe
    Skott via musen

---
+++ SUPShooter/Assets/Wepon.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wepon : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public float fireRate = 0;
+    public float Damage = 10;
+    public LayerMask whatToHit;
+    public Transform BulletTrailPrefab;
+
+    float timeToFire = 0;
+    Transform firePoint;
+
+
+
+
+    void Awake()
+    {
+        firePoint = transform.Find("FirePoint");
+        if(firePoint == null) 
+        {
+            Debug.LogError("no fire point");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fireRate == 0)
+        {
+            if(Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+            else
+            {
+                if (Input.GetButton("Fire1") && Time.time > timeToFire)
+                {
+                    timeToFire = Time.time + 1 / fireRate;
+                    Shoot();
+                }
+            }
+        }
+    }
+    void Shoot()
+    {
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition-firePointPosition, 100, whatToHit);
+        Effect();
+
+        Debug.DrawLine (firePointPosition, (mousePosition-firePointPosition) * 100, Color.cyan) ;
+        Debug.DrawLine(firePointPosition, hit.point, Color.red);
+
+        
+
+    }
+    void Effect()
+    {
+        Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation);
+    }
+}
