e3e40b9f53dc8b73d1f1dd23efaab946d4e79e4a
GustavAldevik <47029227+GustavAldevik@users.noreply.github.com>
2021-04-13 14:28:36 +0200

    arkitektur

---
+++ SUPShooter/Assets/Scripts/HomingBulletScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingBulletScript : MonoBehaviour
+{
+    public float speed = 5;
+    Transform player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+}
---
+++ SUPShooter/Assets/Scripts/Skeleton.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+    public Transform player;
+
+    public GameObject deathEffect;
+
+    public bool isFlipped = false;
+
+    [System.Serializable]
+    public class SkeletonStats
+    {
+        public int Health = 100;
+    }
+    public SkeletonStats stats = new SkeletonStats();
+
+    public void DamageSkeleton(int damage)
+    {
+        stats.Health -= damage;
+        if (stats.Health <= 0)
+        {
+            DeathEffect();
+            GameMaster.KillSkeleton(this);
+
+        }
+    }
+
+    private void DeathEffect()
+    {
+        if (deathEffect != null)
+        {
+            GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
+        }
+    }
+
+
+    public void LookAtPlayer()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if(transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
+    }
+}
