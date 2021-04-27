a579604ae669ae5e77d358aebb28b7cdca89934f
philiplorincz <philip.lorincz@gmail.com>
2021-04-27 14:09:34 +0200

    Timer PhilipsBana

---
+++ SUPShooter/Assets/Scripts/Timercontroller.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Timercontroller : MonoBehaviour
+{
+    public static Timercontroller instans;
+
+    public Text timeCounter;
+
+    private TimeSpan timePlaying;
+    private bool timerGoing;
+    private float eladsedTime;
+
+    private void Awake()
+    {
+        instans = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCounter.text = "Time: 00:00.00";
+        timerGoing = false;
+        BeginTimer();
+    }
+
+    public void BeginTimer() 
+    {
+        timerGoing = true;
+        eladsedTime = 0f;
+        StartCoroutine(UpdateTimer());
+    
+    }
+
+    private void EndTimer() {
+        timerGoing = false;
+    
+    }
+
+    private IEnumerator UpdateTimer() {
+        while (timerGoing)
+        {
+            eladsedTime += Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(eladsedTime);
+            string timePlayingString = "Time: " + timePlaying.ToString("mm':'ss'.'ff");
+            timeCounter.text = timePlayingString;
+
+            yield return null;
+        
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
